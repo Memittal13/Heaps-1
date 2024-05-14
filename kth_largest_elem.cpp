489a45d67f1d47d561c7d7a970831ab1e30865c8
Memittal13 <163784314+Memittal13@users.noreply.github.com>
2024-05-13 20:25:49 -0700

    Create kth_largest_elem.cpp

---
+++ kth_largest_elem.cpp
@@ -0,0 +1,20 @@
+
+//Time O(n log k) :--> iterate over n elements and pq will be of size k(so height of tree)
+//Space O(k)
+//Leetcode: yes
+//issue seen: none
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>>pq;
+
+        for(int i=0; i< nums.size(); i++){
+            pq.push(nums[i]);
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
